0eb8fd2866a2162e2c9fb25ffc12f969bc2e0384
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-02-08 21:51:51 +0900

    chore: eslint 및 prettier 적용

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'prettier'
+  ],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-extra-semi': 'error',
+    'no-tabs': ['error', { 'allowIndentationTabs': true }],
+    'prettier/prettier': 'error',
+  }
+};
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,5 @@
 const Main = () => {
-  return (
-    <div>열린위키 메인 페이지 입니다.</div>
-  );
+  return <div>열린위키 메인 페이지 입니다.</div>;
 };
 
 export default Main;
