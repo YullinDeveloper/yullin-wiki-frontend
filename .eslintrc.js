4a35f1e7ba2413d0bbbda5231cf4b97daa25f06f
이요셉 <iyoseb@iyosebs-MacBook-Pro.local>
2024-02-11 14:53:32 +0900

    fix: window eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,33 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  'overrides': [
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
     {
-      'env': {
-        'node': true
+      env: {
+        node: true,
       },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
-      }
-    }
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'prettier'
-  ],
-  'rules': {
+  plugins: ['prettier'],
+  rules: {
     'react/react-in-jsx-scope': 'off',
-    'indent': ['error', 2],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'no-extra-semi': 'error',
-    'no-tabs': ['error', { 'allowIndentationTabs': true }],
-    'prettier/prettier': 'error',
-  }
+    'no-tabs': ['error', { allowIndentationTabs: true }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 };
