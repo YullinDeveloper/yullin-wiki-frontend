61a62c3493ba2cb499cebcab1eea8124cebc80ca
이요셉 <iyoseb@iyosebs-MacBook-Pro.local>
2024-08-21 20:20:18 +0900

    post페이지 생성

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Post.module.css';
+import Header from '../../components/Header/Header';
 
 const Post = () => {
   return (
     <div id={styles['post']}>
+      <Header />
       Post <br /> 입니다.
     </div>
   );
