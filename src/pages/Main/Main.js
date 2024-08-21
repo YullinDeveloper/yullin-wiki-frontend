1763a2ecc6436b074adedb808ec693415298b0bf
이요셉 <iyoseb@iyosebs-MacBook-Pro.local>
2024-08-21 20:25:01 +0900

    머지과정에서 생긴 오류 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
-import './Main.css';
+import './main.css';
 import MainCardWrapper from '../../components/MainCardWrapper/MainCardWrapper';
 import SmallCardWrapper from '../../components/SmallCardWrapper/SmallCardWrapper';
 
